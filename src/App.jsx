bb94f0e4d5f8dd04b97ff67edc300ff3a5478d6e
Geandre Alfaia <geandreac.dev@gmail.com>
2026-02-22 13:51:01 -0400

    Corrigido erro de importação do App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import About from './components/About';
 import Experience from './components/Experience';
 import Projects from './components/Projects';
 import Skills from './components/Skills';
-import ProfessionalProfile from './components/ProfessionalProfile';
 import ContactForm from './components/ContactForm';
 
 function App() {
@@ -33,7 +32,6 @@ function App() {
         <Experience />
         <Projects />
         <Skills />
-        <ProfessionalProfile />
         <ContactForm />
       </main>
 
